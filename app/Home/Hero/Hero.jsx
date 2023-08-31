7b09b5045d57e6e651ec8cbd1b157860d1c7d08d
Hieu Nguyen <axelnguyen0701@gmail.com>
2023-08-31 09:45:22 +0700

    change a to link, use svg for arrow

--- app/Home/Hero/Hero.jsx
+++ app/Home/Hero/Hero.jsx
@@ -1,5 +1,4 @@
-import Image from "next/image";
-import arrowDown from "../../../public/arrow-204-64.ico";
+import Link from "next/link";
 export default function Hero() {
     return (
         <section
@@ -15,18 +14,35 @@ export default function Hero() {
                 <p>Duy Hảo cam kết có đội ngũ kỹ thuật chuyên nghiệp</p>
                 <p> để phục vụ quý khách trong vòng 24h</p>
             </div>
-            <button className="mt-5 rounded-lg border-2 py-1 px-4 text-duy-hao-purple border-duy-hao-purple border-2 hover:bg-duy-hao-purple hover:text-white">
+            <Link
+                href="/products"
+                className="mt-5 rounded-lg border-2 py-1 px-4 text-duy-hao-purple border-duy-hao-purple border-2 hover:bg-duy-hao-purple hover:text-white"
+            >
                 Sản Phẩm Của Chúng Tôi
-            </button>
+            </Link>
             <div className="relative top-48 text-white">
                 <a href="#about">
-                    <Image
-                        src={arrowDown}
-                        alt="Down Arrow"
-                        priority
-                        height={53}
-                        width={53}
-                    />
+                    <svg
+                        fill="#ffffff"
+                        height="64px"
+                        width="64px"
+                        id="Layer_1"
+                        viewBox="0 0 330 330"
+                        stroke="#ACABAB"
+                    >
+                        <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                        <g
+                            id="SVGRepo_tracerCarrier"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                        ></g>
+                        <g id="SVGRepo_iconCarrier">
+                            <path
+                                id="XMLID_225_"
+                                d="M325.607,79.393c-5.857-5.857-15.355-5.858-21.213,0.001l-139.39,139.393L25.607,79.393 c-5.857-5.857-15.355-5.858-21.213,0.001c-5.858,5.858-5.858,15.355,0,21.213l150.004,150c2.813,2.813,6.628,4.393,10.606,4.393 s7.794-1.581,10.606-4.394l149.996-150C331.465,94.749,331.465,85.251,325.607,79.393z"
+                            ></path>{" "}
+                        </g>
+                    </svg>
                 </a>
             </div>
         </section>
