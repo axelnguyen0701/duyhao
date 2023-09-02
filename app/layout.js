153199d574bd296f1da1a544885eac729f2ab4df
Hieu Nguyen <axelnguyen0701@gmail.com>
2023-09-02 10:37:21 +0700

    add verification google

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,6 @@ import NavBar from "./NavBar/NavBar";
 import Footer from "./Footer/Footer";
 import "./globals.css";
 import { Be_Vietnam_Pro } from "next/font/google";
-import Head from "next/head";
 
 const beVietnamePro = Be_Vietnam_Pro({
     subsets: ["latin"],
@@ -15,17 +14,14 @@ export const metadata = {
         "Welcome to Duy Hao Engineering - Innovating the Future of Engineering Solutions. Chào mừng bạn đến với Duy Hao Engineering - Khám phá tương lai của các giải pháp kỹ thuật",
     keywords:
         "kỹ thuật, giải pháp, sáng tạo, công nghệ, sản xuất, engineering, solutions, innovation, technology, manufacturing",
+    verification: {
+        google: "j5EN_pdEzOUE8P5bmIJTEX1TqqRwBzpEWruVujcRi2g",
+    },
 };
 
 export default function RootLayout({ children }) {
     return (
         <html lang="en">
-            <Head>
-                <meta
-                    name="google-site-verification"
-                    content="j5EN_pdEzOUE8P5bmIJTEX1TqqRwBzpEWruVujcRi2g"
-                />
-            </Head>
             <body className={beVietnamePro.className}>
                 <NavBar />
                 {children}
